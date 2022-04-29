f6894d299dc050a5c9ef08e7a4e187ea6ebdfdee
heyitxtony <heyitxtony@yahoo.com>
2022-04-29 12:27:50 +0000

    update

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ module.exports.each = each;
 
 /**
  * identifty: function takes in any value as an arguemtn and returns that value unchanged
- * @param {any value} value : takes in any valkue as an input 
+ * @param {any value} value : takes in any value as an input 
  * @returns {any} : returns the input value unchanged.
  */
 function identity(value) {
@@ -34,3 +34,26 @@ function identity(value) {
 }
 
 module.exports.identity = identity;
+
+/**
+ * typeof: takes in a input and returns type of dataset
+ * @param {and value} : value: takes in any value input
+ * @return {any}: returns the datatype; i.e. "string", array, boolean, object
+ */
+ _.typeOf = function(value){
+    if (typeof value !== "object"){
+        return typeof value;
+    } else if (typeof value === value){
+     return typeof value;
+    } else if (Array.isArray(value) === true){
+        return "array";
+    } else if (value === null) {
+        return "null";
+    } else if (value instanceof Date){
+        return "date";
+    } else {
+        return "object";
+    }
+}
+
+module.exports.typeOf = typeOf;
